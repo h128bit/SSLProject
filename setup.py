4fe8323373de01d01d29820bd8185862bf0bff57
Асмус Тимофей Андреевич <t.asmus@fgau.local>
2026-01-21 17:51:47 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sslproject",                  
+    version="1.0.0",                          
+    author="h128bit",
+    description="Flex Implementation of self supervised learning methods ",
+    url="https://github.com/h128bit/SSLProject",
+
+    packages=find_packages(),                 
+   
+    install_requires=[
+    "albumentations>=1.3.0",
+    "matplotlib>=3.5.0",
+    "numpy>=2",
+    "pandas>=1.4.0",
+    "Pillow>=9.0.0",        
+    "timm>=0.9.0",
+    "torch>=2.0.0",
+    "torchvision>=0.15.0",
+    "tqdm>=4.60.0",
+    ],
+
+    python_requires=">=3.10",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
